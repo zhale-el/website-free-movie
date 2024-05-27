3e8c3e1addd674536a267021158955cff017b5b5
zhaleh <zhalehelmi@gmail.com>
2024-05-27 17:25:45 +0330

    completed last

---
+++ app/blog/page.jsx
@@ -0,0 +1,41 @@
+const Blog = () => {
+  return (
+    <div className="container mx-auto text-gray-800 w-[80%] ">
+      <p className="text-6xl font-semibold text-blue-950 my-9 text-center">
+        Blog Page
+      </p>
+      <p className="mb-6 justify-evenly ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        laborum vero, maiores unde iste quam quas voluptatum necessitatibus
+        praesentium earum ad sint modi minima exercitationem ipsum laboriosam
+        corporis quidem, eum nisi reprehenderit iure quia. Culpa, molestias.
+        Atque sapiente blanditiis et at laborum sit culpa? Dignissimos doloribus
+        autem fugiat corrupti libero!aborum vero, maiores unde iste quam quas
+        voluptatum necessitatibus praesentium earum ad sint modi minima
+        exercitationem ipsum laboriosam corporis quidem, eum nisi reprehenderit
+        iure quia. Culpa, molestias. Atque sapiente blanditiis et at laborum sit
+        culpa? Dignissimos doloribus autem fugiat corrupti libero!
+      </p>
+      <p className="mb-6 justify-evenly">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+        doloribus! Et non laudantium doloribus, rem, sequi dignissimos eum nulla
+        expedita numquam maxime sapiente, ex distinctio repudiandae obcaecati
+        optio voluptas dolorum. Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Voluptate, doloribus! Et non laudantium doloribus,
+        rem, sequi dignissimos eum nulla expedita numquam maxime sapiente, ex
+        distinctio repudiandae obcaecati optio voluptas dolorum.
+      </p>
+      <p className="mb-9 justify-evenly">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+        doloribus! Et non laudantium doloribus, rem, sequi dignissimos eum nulla
+        expedita numquam maxime sapiente, ex distinctio repudiandae obcaecati
+        optio voluptas dolorum. Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Voluptate, doloribus! Et non laudantium doloribus,
+        rem, sequi dignissimos eum nulla expedita numquam maxime sapiente, ex
+        distinctio repudiandae obcaecati optio voluptas dolorum.
+      </p>
+    </div>
+  );
+};
+
+export default Blog;
